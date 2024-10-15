415ed6fba4861cc03031c3fce8cae9946863d710
fajal-shaikh <fajal.shaikh@cloverinfotech.com>
2024-10-15 11:11:01 +0530

    database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "192.168.180.230",
+  host: "192.168.180.240",
   user: "root",
   password: "Root@123",
   database: 'pharmacy',
